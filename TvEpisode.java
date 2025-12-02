5de42612df6d51c5fecc4723fd409a52bb3bf4f9
jdkenyon <jackdavid04@icloud.com>
2025-12-02 15:50:26 -0600

    Refactor TvEpisode to extend Media with rating

--- TvEpisode.java
+++ TvEpisode.java
@@ -1,56 +1,54 @@
 
-public class TvEpisode extends Media {
+public class TvEpisode extends Media<Integer> {
 
-	private String showTitle;
-	private int seasonNum;
-	private int episodeNum;
-	
-	
-	public TvEpisode(String title, String showTitle, String creator, int year, int seasonNum, int episodeNum, int duration) {
-		super(title, creator, year, duration);
-		setShowTitle(showTitle);
-		setSeasonNum(seasonNum);
-		setEpisodeNum(episodeNum);
-		
-	
-	}
-	
-	public String getShowTitle() {
-		return showTitle;
-	}
+    private String showTitle;
+    private int seasonNum;
+    private int episodeNum;
+    
+    
+    public TvEpisode(String title, String creator, int year, int duration, Integer rating, String showTitle, int seasonNum, int episodeNum) {
+        super(title, creator, year, duration, rating);
+        setShowTitle(showTitle);
+        setSeasonNum(seasonNum);
+        setEpisodeNum(episodeNum);
+    }
+    
+    public String getShowTitle() {
+        return showTitle;
+    }
 
-	public void setShowTitle(String showTitle) {
-		if (showTitle == null || showTitle.isBlank()) {
-			throw new IllegalArgumentException("Show Title cannot be null or blank");
-		}
-		this.showTitle = showTitle;
-	}
-	
-	
-	public int getSeasonNum() {
-		return seasonNum;
-	}
+    public void setShowTitle(String showTitle) {
+        if (showTitle == null || showTitle.isBlank()) {
+            throw new IllegalArgumentException("Show Title cannot be null or blank");
+        }
+        this.showTitle = showTitle;
+    }
+    
+    
+    public int getSeasonNum() {
+        return seasonNum;
+    }
 
-	public void setSeasonNum(int seasonNum) {
-		if (seasonNum < 0) {
-			throw new IllegalArgumentException("Season Number must be greater that 0");
-		}
-		this.seasonNum = seasonNum;
-	}
-	
-	public int getEpisodeNum() {
-		return episodeNum;
-	}
+    public void setSeasonNum(int seasonNum) {
+        if (seasonNum < 0) {
+            throw new IllegalArgumentException("Season Number must be greater that 0");
+        }
+        this.seasonNum = seasonNum;
+    }
+    
+    public int getEpisodeNum() {
+        return episodeNum;
+    }
 
-	public void setEpisodeNum(int episodeNum) {
-		if (episodeNum < 0) {
-			throw new IllegalArgumentException("Episode Num must be greater that 0");
-		}
-		this.episodeNum = episodeNum;
-	}
-	
-	@Override
-	public String toString() {
-		return String.format("Title: %s ShowTitle: %s Creator: %s  Year: %d Season Number: %d Episode Number: %d Duration: %d Rating: %d", getTitle(), showTitle, getCreator(), getYear(), seasonNum, episodeNum, getDuration(), getRating());
-	}
+    public void setEpisodeNum(int episodeNum) {
+        if (episodeNum < 0) {
+            throw new IllegalArgumentException("Episode Num must be greater that 0");
+        }
+        this.episodeNum = episodeNum;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Title: %s ShowTitle: %s Creator: %s  Year: %d Season Number: %d Episode Number: %d Duration: %d Rating: %d", getTitle(), showTitle, getCreator(), getYear(), seasonNum, episodeNum, getDuration(), getRating());
+    }
 }
